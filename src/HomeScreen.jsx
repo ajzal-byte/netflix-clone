7858133afdd22386b5f7c10bfd328ebb8bde01e4
ajzal-byte <ajzalaju10@gmail.com>
2024-03-01 06:44:07 +0530

    creating components

---
+++ src/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import "./HomeScreen.css";
+import { Nav } from "./components";
+
+const HomeScreen = () => {
+  return (
+    <div className='homeScreen'>
+        HomeScreen
+        <Nav />
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6T_6bw2ugWUoK-Q3oPuh1cettdq1hVlY",
+  authDomain: "netflix-clone-dd2df.firebaseapp.com",
+  projectId: "netflix-clone-dd2df",
+  storageBucket: "netflix-clone-dd2df.appspot.com",
+  messagingSenderId: "330452078524",
+  appId: "1:330452078524:web:812b1f986fefba60e425b9"
+};
