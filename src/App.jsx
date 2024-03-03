562a0e7f7e2930a2538eb0b63fd1440d4586f5aa
ajzal-byte <ajzalaju10@gmail.com>
2024-03-03 13:56:15 +0530

    login and logout updated using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,32 @@ import {HomeScreen, LoginPage} from "./pages"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from 'react-hot-toast';
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import {login, logout, selectUser} from "./features/userSlice"
 
 function App() {
-  const user = null;
+  // const user = "ajzal"
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(userAuth => {
       if (userAuth) {
         // Logged in
-      }else{
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }))
+      } else {
         // Logged out
+        dispatch(logout);
       }
-      return unsubscribe;
-    },[])
-  })
+    });
+
+    return unsubscribe;
+  }, [dispatch]); // <- This array is important for useEffect, it's a dependency array
+
+
   return (
     <div className="app">
       <div><Toaster/></div>
